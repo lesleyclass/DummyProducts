507f880102b951030cec0630f8638a663ac8b068
lesleyferreira <lesley.ferreira.tw@naturapay.net>
2024-02-18 18:34:19 -0300

    refactor: refatora a chamada do endpoint de produtos

--- app/src/main/java/br/edu/ifsp/scl/sdm/dummyproducts/ui/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/dummyproducts/ui/MainActivity.kt
@@ -6,14 +6,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import br.edu.ifsp.scl.sdm.dummyproducts.R
 import br.edu.ifsp.scl.sdm.dummyproducts.adapter.ProductAdapter
 import br.edu.ifsp.scl.sdm.dummyproducts.adapter.ProductImageAdapter
 import br.edu.ifsp.scl.sdm.dummyproducts.databinding.ActivityMainBinding
+import br.edu.ifsp.scl.sdm.dummyproducts.model.DummyJSONAPI
 import br.edu.ifsp.scl.sdm.dummyproducts.model.Product
 import br.edu.ifsp.scl.sdm.dummyproducts.model.ProductList
+import com.android.volley.Request
+import com.android.volley.toolbox.ImageRequest
+import com.android.volley.toolbox.StringRequest
 import com.google.gson.Gson
 import com.google.gson.JsonSyntaxException
 import kotlinx.coroutines.CoroutineScope
@@ -78,60 +83,33 @@ class MainActivity : AppCompatActivity() {
         retrieveProducts()
     }
 
-    private fun retrieveProducts() = Thread {
-        val productsConnection = URL(PRODUCT_ENDPOINT).openConnection() as HttpURLConnection
-        try {
-            if (productsConnection.responseCode == HTTP_OK){
-                InputStreamReader(productsConnection.inputStream).readText().let {
-                    runOnUiThread {
-                        productAdapter.addAll(Gson().fromJson(it, ProductList::class.java).products)
-                    }
+    private fun retrieveProducts() =
+        DummyJSONAPI.ProductListRequest(
+            { productList ->
+                productList.products.also {
+                    productAdapter.addAll(it)
                 }
-            } else {
-                runOnUiThread {
-                    Toast.makeText(this, getString(R.string.request_problem), Toast.LENGTH_SHORT)
-                        .show()
-                }
-            }
-        } catch (ioe: IOException){
-            runOnUiThread {
-                Toast.makeText(this, getString(R.string.connection_failed), Toast.LENGTH_SHORT)
-                    .show()
-            }
-        } catch (jse: JsonSyntaxException){
-            runOnUiThread {
-                Toast.makeText(this, getString(R.string.response_problem), Toast.LENGTH_SHORT)
-                    .show()
-            }
-        } finally {
-            productsConnection.disconnect()
-        }
-    }.start()
-    private fun retrieveProductImages(product: Product) = Thread {
-        product.images.forEach { imageUrl ->
-            val imageConnection = URL(imageUrl).openConnection() as HttpURLConnection
+            },
+            {
+                Toast.makeText(this, getString(R.string.request_problem), Toast.LENGTH_SHORT).show()
+            },
+        ).also { DummyJSONAPI.getInstance(this).addToRequestQueue(it) }
 
-            try {
-                if (imageConnection.responseCode == HTTP_OK) {
-                    BufferedInputStream(imageConnection.inputStream).let {
-                        val imageBitmap = BitmapFactory.decodeStream(it)
-                        runOnUiThread {
-                            productImageList.add(imageBitmap)
-                            productImageAdapter.notifyItemInserted(productImageList.lastIndex)
-                        }
-                    }
-                } else {
-                    runOnUiThread {
-                        Toast.makeText(this, getString(R.string.request_problem), Toast.LENGTH_SHORT).show()
-                    }
-                }
-            } catch (ioe: IOException) {
-                runOnUiThread {
-                    Toast.makeText(this, getString(R.string.connection_failed), Toast.LENGTH_SHORT).show()
-                }
-            } finally {
-                imageConnection.disconnect()
-            }
+    private fun retrieveProductImages(product: Product) =
+        product.images.forEach { imageUrl ->
+            ImageRequest(
+                imageUrl,
+                { response ->
+                    productImageList.add(response)
+                    productImageAdapter.notifyItemInserted(productImageList.lastIndex)
+                },
+                0,
+                0,
+                ImageView.ScaleType.CENTER,
+                Bitmap.Config.ARGB_8888,
+                {
+                    Toast.makeText(this, getString(R.string.response_problem), Toast.LENGTH_SHORT).show()
+                },
+            ).also { DummyJSONAPI.getInstance(this).addToRequestQueue(it) }
         }
-    }.start()
 }
